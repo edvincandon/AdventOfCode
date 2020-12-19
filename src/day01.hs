34e12f62768e0e41a17b4301ad310d64c22c5d3b
Edvin CANDON <edvincandon@gmail.com>
2020-12-19 11:00:54 +0100

    forgot day01 part 2

--- src/day01.hs
+++ src/day01.hs
@@ -1,6 +1,11 @@
 main = do
   contents <- readFile "./data/day01.txt"
   print . findTupleResult . map read . words $ contents
+  print . findTripleResult . map read . words $ contents
 
 findTupleResult :: [Int] -> Int
 findTupleResult x = product $ head [ [a, b] | a <- x, b <- x, a + b == 2020 ]
+
+findTripleResult :: [Int] -> Int
+findTripleResult x =
+  product $ head [ [a, b, c] | a <- x, b <- x, c <- x, a + b + c == 2020 ]
