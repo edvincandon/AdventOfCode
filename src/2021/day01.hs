570ccd0ad3118e1dab4de80d56622581def5313e
Edvin CANDON <edvincandon@gmail.com>
2021-12-02 15:52:06 +0100

    2021: day 01

---
+++ src/2021/day01.hs
@@ -0,0 +1,18 @@
+main :: IO ()
+main = do
+  depths <- lines <$> readFile "./src/2021/data/day01.txt"
+  putStrLn $ "Part 1 = " ++ show (countIncreases . map readInt $ depths)
+  putStrLn $ "Part 2 = " ++ show
+    (countIncreases . computeWindow . map readInt $ depths)
+
+readInt :: String -> Int
+readInt = read
+
+countPos :: [Int] -> Int
+countPos = length . filter (> 0)
+
+countIncreases :: [Int] -> Int
+countIncreases xs = countPos (zipWith (-) (tail xs) xs)
+
+computeWindow :: [Int] -> [Int]
+computeWindow xs = zipWith (+) (zipWith (+) (tail $ tail xs) (tail xs)) xs
