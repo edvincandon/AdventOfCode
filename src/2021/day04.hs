5876c2e590acd348f6b357ce37a9bff83b8ba0e6
Edvin CANDON <edvincandon@gmail.com>
2021-12-05 14:47:09 +0100

    2021: day04

---
+++ src/2021/day04.hs
@@ -0,0 +1,141 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+import qualified Data.Map                      as M
+import           Data.Char                      ( isSpace )
+import           Data.List                      ( groupBy
+                                                , find
+                                                )
+import           Data.List.Split.Internals      ( splitOn )
+import           Control.Monad.State            ( runState
+                                                , MonadState(put, get)
+                                                , State
+                                                )
+
+
+
+main :: IO ()
+main = do
+  rawData <- lines <$> readFile "./src/2021/data/day04.txt"
+  let nums   = parseNums $ head rawData
+  let boards = toBingoState 5 $ tail rawData
+  putStrLn "---Part 1 -------------"
+  let bingo = runState (runGame nums)
+                       GameState { boards = boards, result = Nothing }
+  print $ computeScore $ fst bingo
+  putStrLn "---Part 2 -------------"
+  let bingo = runState (runGame2 nums)
+                       GameState { boards = boards, result = Nothing }
+  print $ computeScore $ fst bingo
+
+
+-- Types --
+type Board = M.Map Int ((Int, Int), Bool)
+type Bingo = Maybe Board
+type AxisCounter = M.Map Int Int
+type BingoResult = (Bingo, Int)
+
+data BoardState = BoardState
+  { board :: Board
+  , rows  :: AxisCounter
+  , cols  :: AxisCounter
+  , bingo :: Bool
+  }
+  deriving Show
+
+data GameState = GameState
+  { boards :: [BoardState]
+  , result :: Bingo
+  }
+  deriving Show
+
+
+-- Helpers --
+readInt :: String -> Int
+readInt = read
+
+parseNums :: String -> [Int]
+parseNums = fmap readInt . splitOn ","
+
+groupByN :: Int -> [a] -> [[a]]
+groupByN _ [] = []
+groupByN n xs | n > 0     = take n xs : groupByN n (drop n xs)
+              | otherwise = error "invalid group arguments"
+
+createCoords :: (Int, Int) -> [(Int, Int)]
+createCoords (n, m) = zip (take (n * n) $ cycle $ take n [0 ..])
+                          (concatMap (replicate m) $ take m [0 ..])
+
+createAxisCounter :: Int -> AxisCounter
+createAxisCounter n = M.fromList $ zip [0 ..] (replicate n 0)
+
+toBingoState :: Int -> [String] -> [BoardState]
+toBingoState n xs =
+  map
+      ( (\board -> BoardState { board = board
+                              , rows  = createAxisCounter n
+                              , cols  = createAxisCounter n
+                              , bingo = False
+                              }
+        )
+      . M.fromList
+      . flip zip (zip (createCoords (n, n)) (repeat False))
+      . concatMap (map readInt . filter (not . all isSpace) . splitOn " ")
+      )
+    $ groupByN n (filter (not . all isSpace) xs)
+
+
+-- Part 1 --
+doBingoTurn :: Int -> BoardState -> BoardState
+doBingoTurn n state@(BoardState board rows cols _) = BoardState b r c _bingo
+ where
+  b      = M.adjust (\(coords, _) -> (coords, True)) n board
+  match  = M.lookup n board
+  (r, c) = case match of
+    Nothing          -> (rows, cols)
+    Just ((x, y), _) -> (M.adjust (+ 1) y rows, M.adjust (+ 1) x cols)
+  fullRow = filter (\(_, count) -> count == 5) $ M.toList r
+  fullCol = filter (\(_, count) -> count == 5) $ M.toList c
+  _bingo  = length fullRow == 1 || length fullCol == 1
+
+
+isBoardBingo :: BoardState -> Bool
+isBoardBingo (BoardState _ _ _ bingo) = bingo
+
+findBingo :: [BoardState] -> Bingo
+findBingo xs = (\s@(BoardState board _ _ _) -> board) <$> find isBoardBingo xs
+
+computeScore :: BingoResult -> Maybe Int
+computeScore (board, last) =
+  (* last)
+    .   foldl (\acc (val, _) -> val + acc) 0
+    .   filter (\(_, (_, visited)) -> not visited)
+    .   M.toList
+    <$> board
+
+runGame :: [Int] -> State GameState BingoResult
+
+runGame [] = do
+  return (Nothing, -1)
+runGame (x : xs) = do
+  GameState { boards, result } <- get
+  let _boards = map (doBingoTurn x) boards
+  let _bingo  = findBingo _boards
+  put $ GameState { boards = _boards, result = _bingo }
+  maybe (runGame xs) (return . (\b -> (Just b, x))) _bingo
+
+
+-- Part 2 --
+runGame2 :: [Int] -> State GameState BingoResult
+
+runGame2 [] = do
+  return (Nothing, -1)
+runGame2 (x : xs) = do
+  GameState { boards, result } <- get
+  let _boards = map (doBingoTurn x) boards
+  let rest = if length _boards == 1
+        then _boards
+        else filter (not . isBoardBingo) _boards
+  let _bingo = findBingo rest
+  put $ GameState { boards = rest, result = _bingo }
+  maybe (runGame2 xs) (return . (\b -> (Just b, x))) _bingo
+
