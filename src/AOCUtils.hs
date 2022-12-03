8f78dbd346a8c5c9af0a011718438d985cd5263c
Edvin CANDON <edvincandon@gmail.com>
2022-12-03 10:38:20 +0100

    add tupleToList util

--- src/AOCUtils.hs
+++ src/AOCUtils.hs
@@ -1,12 +1,9 @@
 module AOCUtils where
-import           Data.List.Split                ( splitOn )
-import           Data.List                      ( sortOn
-                                                , groupBy
-                                                , sort
-                                                , group
-                                                , genericLength
-                                                )
-import           Data.Maybe                     ( fromMaybe )
+
+import           Data.List       (genericLength, group, groupBy, sort, sortOn)
+import           Data.List.Split (splitOn)
+import           Data.Maybe      (fromMaybe)
+
 -- Helpers --
 -- DATA PARSING --
 readInt :: String -> Int
@@ -25,49 +22,57 @@ median [] = error "median impossible"
 median xs
   | length xs == 1 = realToFrac $ head xs
   | odd split = realToFrac $ (!!) sorted $ (split - 1) `div` 2
-  | otherwise = realToFrac
-  $ mean [(!!) sorted $ split `div` 2 - 1, (!!) sorted $ split `div` 2]
- where
-  sorted = sort xs
-  split  = genericLength sorted
+  | otherwise =
+    realToFrac $
+    mean [(!!) sorted $ split `div` 2 - 1, (!!) sorted $ split `div` 2]
+  where
+    sorted = sort xs
+    split = genericLength sorted
 
 -- LISTS --
 toIndexedList :: [a] -> [(a, Int)]
 toIndexedList = flip zip [0 ..]
 
 safeIndex :: [a] -> Int -> Maybe a
-safeIndex xs i | (i > -1) && (length xs > i) = Just (xs !! i)
-               | otherwise                   = Nothing
+safeIndex xs i
+  | (i > -1) && (length xs > i) = Just (xs !! i)
+  | otherwise = Nothing
 
 safeIndex1 :: [a] -> Int -> a -> a
 safeIndex1 xs i d = fromMaybe d (safeIndex xs i)
 
 groupByN :: Int -> [a] -> [[a]]
 groupByN _ [] = []
-groupByN n xs | n > 0     = take n xs : groupByN n (drop n xs)
-              | otherwise = error "invalid group arguments"
+groupByN n xs
+  | n > 0 = take n xs : groupByN n (drop n xs)
+  | otherwise = error "invalid group arguments"
 
 replaceNth :: Int -> a -> [a] -> [a]
 replaceNth _ _ [] = []
-replaceNth n newVal (x : xs) | n == 0    = newVal : xs
-                             | otherwise = x : replaceNth (n - 1) newVal xs
+replaceNth n newVal (x:xs)
+  | n == 0 = newVal : xs
+  | otherwise = x : replaceNth (n - 1) newVal xs
 
--- BINARY -- 
+splitHalf :: [a] -> ([a], [a])
+splitHalf xs = splitAt ((length xs + 1) `div` 2) xs
+
+-- BINARY --
 bin2num :: [Int] -> Int
 bin2num list = parse (reverse list) 0
- where
-  parse []       _ = 0
-  parse (x : xs) n = x * (2 ^ n) + parse xs (n + 1)
+  where
+    parse [] _     = 0
+    parse (x:xs) n = x * (2 ^ n) + parse xs (n + 1)
 
 -- TUPLES --
+tupleToList :: (a, a) -> [a]
+tupleToList (a, b) = [a, b]
+
 uniqueTuples :: (Ord a, Ord b) => [(a, b)] -> [(a, b)]
 uniqueTuples = minUniqueTuples 1
 
 minUniqueTuples :: (Ord a, Ord b) => Int -> [(a, b)] -> [(a, b)]
 minUniqueTuples minRep =
-  fmap head
-    <$> filter ((>= minRep) . length)
-    .   concatMap (groupBy (\a b -> snd a == snd b) . sortOn snd)
-    .   groupBy (\a b -> fst a == fst b)
-    .   sortOn fst
-
+  fmap head <$>
+  filter ((>= minRep) . length) .
+  concatMap (groupBy (\a b -> snd a == snd b) . sortOn snd) .
+  groupBy (\a b -> fst a == fst b) . sortOn fst
